b1a365b23e3541fd0ed013667613e9dc881bc638
Je-el <jewelmaldonado@Jewels-MacBook-Air.local>
2023-11-02 01:30:17 -0800

    Finally compiles and runs with all req's passed

--- Assignments/Assignment_3/da3.cpp
+++ Assignments/Assignment_3/da3.cpp
@@ -1,54 +1,35 @@
-//Jewel Maldonado
-//CS311
-//2023-09-25
-//da3.cpp
-
+// da3.hpp
+// Author: Jewel Maldonado
+// Last Edit Date: 2023-11-02
+// Purpose: Source file containing the implementation of da3.hpp
 #include "da3.hpp"
-#include <functional>
-using std::function;
-#include <stdexcept>
-#include <algorithm>
-#include "llnode.hpp"
 
-
-// Exercise A - Linked List Lookup
-template <typename ValueType>
-ValueType lookup(const LLNode<ValueType> * head, std::size_t index) {
-    while (head != nullptr && index > 0) {
-        head = head->next;
-        index--;
+//Exercise C
+void didItThrow(const std::function<void()>& ff, bool& threw) {
+    // assume no exception will be thrown
+    threw = false;
+    try {
+        // Attempt to call the function
+        ff();
+    } catch (...) {
+        // If an exception is caught, set threw to true and re-throw
+        threw = true;
+        throw; // Re-throw the current exception without handling it here
     }
-
-    if (head == nullptr) {
-        throw std::out_of_range("Index out of range.");
-    }
-    return head->data;
-}
-
-// Exercise B - Did It Throw?
-void didItThrow(const function<void()> & ff, bool & threw)
-{
-  threw=false;
-  try{
-    ff();
-  }
-   catch(...)
-  { 
-    threw = true;
-    throw;
-    }
- }
-
-// Exercise C - Check Sorted
-template <typename FDIter>
-bool checkSorted(FDIter first, FDIter last) {
-    return std::is_sorted(first, last);
 }
 
 // Exercise D - Recursive GCD
-int gcd(int a, int b) {
-    if (a == 0) return b;
-    if (b == 0) return a;
+// Helper function for gcd that is hidden from the client code
+// Not exposed in the header file, hence not callable by the client code
+namespace {
+    int gcdRecursive(int a, int b) {
+        if (b == 0) return a;  // Base case: if b is 0, then a is the GCD
+        return gcdRecursive(b, a % b);  // Recursive case: gcd(a, b) = gcd(b, a % b)
+    }
+}
 
-    return gcd(b, a % b);
+// Public gcd function that client code will call
+int gcd(int a, int b) {
+    // Assuming a and b are nonnegative and not both zero.
+    return gcdRecursive(a, b);
 }
--- Assignments/Assignment_3/da3.hpp
+++ Assignments/Assignment_3/da3.hpp
@@ -1,28 +1,54 @@
-//Jewel Maldonado
-//CS311
-//2023-09-25
-//da3.hpp
+// da3.hpp
+// Author: Jewel Maldonado
+// Last Edit Date: 2023-11-02
+// Purpose: Header file containing function templates for Assignment 3
 
 #ifndef DA3_HPP
 #define DA3_HPP
 
-#include <functional>
-using std::function;
-#include <stdexcept>
-#include "llnode.hpp"
+#include <stdexcept>  // Includes standard exception classes like std::out_of_range
+#include <cstddef>    // Includes definitions for size_t and other types
+#include "llnode.hpp" // Includes the LLNode class template definition for linked list nodes
+#include <functional> // Includes std::function to allow passing functions as arguments
+#include <algorithm>  // Includes algorithms like std::is_sorted
+#include <string>     // Includes std::string class and functions like std::to_string
+//-------------------------------------------------------------------------------------//
+//-------------------------------------------------------------------------------------//
 
-// Exercise A - Linked List Lookup
+// Exercise A
+// Function template to look up the nth element in a linked list
+// Preconditions: The list must be null-terminated and index must be within the bounds of the list.
+// Throws: std::out_of_range if the index is out of range.
 template <typename ValueType>
-ValueType lookup(const LLNode<ValueType> * head, std::size_t index);
+ValueType lookup(const LLNode<ValueType>* head, std::size_t index) {
+    const LLNode<ValueType>* current = head;
+    std::size_t currentIndex = 0;
 
-// Exercise B - Did It Throw?
-void didItThrow(const function<void()> & ff, bool & threw);
+    // Traversal the linked list until the end or the specified index is reached
+    while (current != nullptr) {
+        if (currentIndex == index) {
+            return current->_data; // Once found, return the data
+        }
+        current = current->_next;
+        ++currentIndex;
+    }
 
-// Exercise C - Check Sorted
+    // If the specified index is not found in the list, throw out-of-range exception
+    throw std::out_of_range("Index " + std::to_string(index) + " is out of range for linked list lookup");
+}
+
+// Exercise B
+// Function prototype for didItThrow
+// This function calls another function and says whether it threw an exception.
+void didItThrow(const std::function<void()>& ff, bool& threw);
+
+// Exercise C
 template <typename FDIter>
-bool checkSorted(FDIter first, FDIter last);
+bool checkSorted(FDIter first, FDIter last) {
+    return std::is_sorted(first, last);
+}
 
-// Exercise D - Recursive GCD
-int gcd(int a, int b);
+// Excercise D
+int gcd(int a, int b) ;
 
-#endif  // DA3_HPP
+#endif // DA3_HPP
